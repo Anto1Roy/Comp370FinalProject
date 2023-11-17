0d841d36ed2915e9016676ee6078d2c4f293025f
Dun Niu <dunniu2003@gmail.com>
2023-11-16 22:31:47 -0500

    Modify script to fetch articles
    Cache all 5 pages of all the request. Create a csv with the useful
    columns + 2 more used for coding.

--- src/newsapi_test.py
+++ src/newsapi_test.py
@@ -4,18 +4,19 @@ import argparse
 from pathlib import Path
 import pandas as pd
 
-def fetch_articles():
+def fetch_articles(page):
     base_url = 'https://newsapi.org/v2/everything'
 
-    sources_id = json.load(open(Path(__file__).parent.parent / 'data/news_source/sources_id.json', 'r'))['id'][:20]
+    sources_id = json.load(open(Path(__file__).parent.parent / 'data/news_source/sources_id.json', 'r'))['id']
     query_params = {
         'apiKey': args.api_key,
         'sources': ",".join(map(str, sources_id)),
         'q': 'taylor swift',
         'language': 'en',
+        'page': page
     }
 
-    source_path = Path(__file__).parent.parent / 'data/articles/cache/articles.json'
+    source_path = Path(__file__).parent.parent / f'data/articles/cache/articles_{page}.json'
     if not source_path.exists():
         try:
             response = requests.get(base_url, params=query_params)
@@ -29,13 +30,22 @@ def fetch_articles():
             print(f"Error fetching articles: {e}")
     else:
         return(json.load(open(source_path, 'r')))
+    
 
 def main():
-    articles = fetch_articles()["articles"]
+    articles = []
+    for page in range(1, 6):
+        response = fetch_articles(page)
+        articles += response['articles']
+
     for i in articles:
         i['source'] = i['source']['name']
         del i['content']
+        del i['urlToImage']
+        del i['publishedAt']
     df = pd.DataFrame(articles)
+    df['category'] = ''
+    df['coverage'] = ''
     df.to_csv(Path(__file__).parent.parent / 'data/articles/articles.csv', index=False)
 
 
