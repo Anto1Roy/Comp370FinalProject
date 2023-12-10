305a7fcb932da29015800464c3031c11c2a3acf9
Anto1Roy <antonin1roy@hotmail.com>
2023-12-10 17:51:23 -0500

    Fix output file name in compute_results_by_category.py

--- scripts/compute_results_by_category.py
+++ scripts/compute_results_by_category.py
@@ -10,10 +10,11 @@ def analyze_csv(input_csv, output_json):
     df['score'] = pd.to_numeric(df['score'], errors='coerce')
 
     # Compute average score and standard deviation for each category
-    group_stats = df.groupby('category')['score'].agg(['mean', 'std', 'avg']).reset_index()
+    #group_stats = df.groupby('category')['score'].agg(['mean', 'std', 'avg']).reset_index()
+    group_stats = df['score'].agg(['mean', 'std'])
 
     # Convert group_stats DataFrame to a dictionary for JSON output
-    stats_dict = group_stats.set_index('category').to_dict(orient='index')
+    stats_dict = group_stats.to_dict()
 
     # Save the results to a JSON file
     with open(output_json, 'w') as json_file:
@@ -22,7 +23,7 @@ def analyze_csv(input_csv, output_json):
 if __name__ == "__main__":
     # Replace 'your_input_file.csv' and 'output_stats.json' with your actual file names
     input_file = os.path.join(os.path.dirname(__file__), '../data/articles/articles.csv')
-    output_json = os.path.join(os.path.dirname(__file__), '../data/articles/results/stats_per_category.json')
+    output_json = os.path.join(os.path.dirname(__file__), '../data/articles/results/stats_overall.json')
 
     # Analyze the CSV file and save results to JSON
     analyze_csv(input_file, output_json)
